08158f768197664d4b76d87f132a40e8ad5a63ad
malikawais <awais@example.com>
2021-06-12 20:04:28 +0500

    Day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  //   int days = 30;
+  // String name = "codepur";
+
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
