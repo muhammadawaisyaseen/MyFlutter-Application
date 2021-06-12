8b59b2b7b0e0723d548bdcc9d1dbb263f775f43f
malikawais <awais@example.com>
2021-06-13 02:59:31 +0500

    Day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
-import 'home_page.dart';
+import 'package:flutter_catalog/login_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +11,19 @@ class MyApp extends StatelessWidget {
   //   int days = 30;
   // String name = "codepur";
 
-  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.pink),
+      darkTheme: ThemeData(primarySwatch: Colors.brown),
+      initialRoute: "/loginpage",
+      routes: {
+        '/': (context) => LoginPage(),
+        "/loginpage": (context) => LoginPage(),
+        "/homepage": (context) => HomePage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,6 +7,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: Color.fromARGB(78, 93, 43, 12),
       appBar: AppBar(
         title: Text("Flutter Catalog"),
       ),
