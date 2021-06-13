b58463fc79a07c9ce82ed5bbee7799c82fb81ac3
malikawais <awais@example.com>
2021-06-13 19:17:52 +0500

    Day4 completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -4,12 +4,56 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              color: Colors.red, fontWeight: FontWeight.bold, fontSize: 40),
-        ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "myassets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 40),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: "username"),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ))),
+                    onPressed: () {
+                      print("Awais yaseen");
+                    },
+                    child: Text("Login")),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/login_page.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,9 +17,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.pink),
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        fontFamily: GoogleFonts.padauk().fontFamily,
+      ),
       darkTheme: ThemeData(primarySwatch: Colors.brown),
-      initialRoute: "/loginpage",
+      initialRoute: "/",
       routes: {
         '/': (context) => LoginPage(),
         "/loginpage": (context) => LoginPage(),
